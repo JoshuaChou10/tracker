64c5411be30be6d59a5f9f95fc2133727b259e99
Joshua Chou <joshuachou912@gmail.com>
2024-06-19 19:20:40 -0400

    Created easter egg

--- app/page.tsx
+++ app/page.tsx
@@ -174,7 +174,11 @@ const handleNoteChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
       }
     return "text-2xl"
     };
-    
+    const Hide=()=>{
+      setNote('')
+      setTracking('')
+
+    }
   
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -200,7 +204,7 @@ const handleNoteChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
 </div>
 
       <div className="fixed left-0 top-0 p-4 bg-gray-800 text-white rounded-lg shadow-lg">
-  <h2 className="text-lg font-bold mb-2">What are you tracking?</h2>
+  <h2 className="text-lg font-bold mb-2">What are you Tracking? </h2>
   <input
     type="text"
     value={tracking}
@@ -224,7 +228,7 @@ const handleNoteChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     className="ml-2 w-16 p-1 bg-gray-700 text-white rounded mb-4"
   />
 </div>
-<h1 className="text-2xl">{tracking}</h1>
+<h1     onClick={Hide} className="text-2xl">{tracking}</h1>
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
 
         <Image
@@ -233,6 +237,7 @@ const handleNoteChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
           alt="Tracker Logo"
           width={200}
           height={42}
+      
           priority
         />
       </div>
